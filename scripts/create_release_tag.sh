b8664a11b220847a48541731565ba597dcfb576e
Timofey Kirillov <timofey.kirillov@flant.com>
2019-12-25 13:02:57 +0300

    [ci] Create experimental releases on pushes to `experimental` branch

--- scripts/create_release_tag.sh
+++ scripts/create_release_tag.sh
@@ -12,26 +12,33 @@ if [ -z "${1-}" ] ; then
   exit 1
 fi
 
-if [ "$1" = ":experimental" ] ; then
-  BINTRAY_REPO=multiwerf-experimental
-  VERSION="v$(date +%y.%m.%d-%H.%M.%S)"
-else
-  BINTRAY_REPO=multiwerf-nonexistent # FIXME: remove durak-guard just before release when master will be ready for stable
-  VERSION=$1
-fi
-
 DIR="$(dirname "${0}")"
+CHANGELOG_TEXT=""
+EXTRA_GIT_TAG_OPTS=""
+
+if [ "$1" = ":experimental" ] ; then
+  VERSION="v$(date +%y.%m.%d-%H.%M.%S)"
+  BINTRAY_REPO=multiwerf-experimental
+
+  echo "Creating experimental release version $VERSION"
+
+  CHANGELOG_TEXT="Experimental release."
+else
+  VERSION=$1
+  BINTRAY_REPO=multiwerf-nonexistent # FIXME: remove durak-guard just before release when master will be ready for stable
+
+  LATEST_TAG="$(git tag -l --sort=-taggerdate | head -n1)"
+  CHANGELOG_TEXT="$(git log --pretty="%s" HEAD...${LATEST_TAG})"
+  if [[ -n $CHANGELOG_TEXT ]] ; then
+    CHANGELOG_TEXT="$(echo "$CHANGELOG_TEXT" | grep -v '^Merge' | sed 's/^/- /')"
+  fi
+  EXTRA_GIT_TAG_OPTS="--edit"
+
+  echo "Creating release version $VERSION"
+fi
 
 TAG_TEMPLATE="$DIR/git_tag_template.md"
 
-LATEST_TAG="$(git tag -l --sort=-taggerdate | head -n1)"
-echo "latest tag is ${LATEST_TAG}"
-CHANGELOG_TEXT="$(git log --pretty="%s" HEAD...${LATEST_TAG})"
-if [[ -n $CHANGELOG_TEXT ]] ; then
-  CHANGELOG_TEXT="$(echo "$CHANGELOG_TEXT" | grep -v '^Merge' | sed 's/^/- /')"
-fi
-echo "CHANGELOG_TEXT = ${CHANGELOG_TEXT}"
-
-BINTRAY_REPO="${BINTRAY_REPO}" VERSION="${VERSION}" CHANGELOG_TEXT="${CHANGELOG_TEXT}" envsubst < ${TAG_TEMPLATE} | git tag --annotate --file - --edit $VERSION
+BINTRAY_REPO="${BINTRAY_REPO}" VERSION="${VERSION}" CHANGELOG_TEXT="${CHANGELOG_TEXT}" envsubst < ${TAG_TEMPLATE} | git tag --annotate --file - $EXTRA_GIT_TAG_OPTS $VERSION
 
 git push --tags
