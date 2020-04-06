ea4a18702be6a86711d499a1d6c0ce1ba985c93c
Alexey Igrychev <alexey.igrychev@flant.com>
2020-04-06 17:32:08 +0100

    +

--- pkg/util/setsid.go
+++ pkg/util/setsid.go
@@ -4,18 +4,22 @@ package util
 
 import (
 	"fmt"
+	"os"
 	"syscall"
 )
 
 func Setsid() error {
 	fmt.Println("!!!")
-
+	fmt.Println(os.Getpid())
+	fmt.Println(os.Getppid())
 	pid, err := syscall.Setsid()
 	if pid == -1 || err != nil {
 		return err
 	}
 
 	fmt.Println("!!!")
+	fmt.Println(os.Getpid())
+	fmt.Println(os.Getppid())
 
 	return nil
 }
