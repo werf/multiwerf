4713dcd86406f417848b5b505fb2589f11166380
Alexey Igrychev <alexey.igrychev@flant.com>
2020-04-06 17:53:45 +0100

    +

--- pkg/util/setsid.go
+++ pkg/util/setsid.go
@@ -9,6 +9,10 @@ import (
 )
 
 func Setsid() error {
+	fmt.Println(syscall.Getuid())
+	fmt.Println(syscall.Getpid())
+	fmt.Println(syscall.Getppid())
+
 	ret, _, errno := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
 	if errno != 0 {
 		return fmt.Errorf("fork failed: errno %d", errno)
@@ -21,6 +25,10 @@ func Setsid() error {
 	if pid < 0 || err != nil {
 		return err
 	}
+
+	fmt.Println(syscall.Getuid())
+	fmt.Println(syscall.Getpid())
+	fmt.Println(syscall.Getppid())
 
 	return nil
 }
